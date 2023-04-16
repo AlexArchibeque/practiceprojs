e99753e897c56c2c3e6a0435886e8ac130233835
AlexArchibeque <wonkman33@gmail.com>
2023-04-16 14:39:07 -0700

    almost all of todo

--- src/pages/todo/index.tsx
+++ src/pages/todo/index.tsx
@@ -1,7 +1,71 @@
-export default function Todo() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      This is the todo list
-    </div>
-  )
+import React from "react";
+
+interface TodoList {
+  [key: string]: boolean;
 }
+
+export default function Todo() {
+  const [todoList, setTodoList] = React.useState<TodoList>({});
+  const [newTodo, setNewTodo] = React.useState("");
+  const [todoError, setTodoError] = React.useState("");
+
+  const addTodo = () => {
+    if (todoList[newTodo] !== undefined) {
+      setTodoError("Already in todos :)");
+    } else {
+      todoList[newTodo] = false;
+      setNewTodo("");
+    }
+  };
+
+  const handleRadioClick = () => {};
+
+  return (
+    <div className="flex min-h-screen flex-col items-center p-24">
+      <div className="flex justify-between mb-1 w-3/12">
+        <input
+          value={newTodo}
+          className="text-black"
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button
+          onClick={addTodo}
+          className="bg-slate-700 hover:bg-slate-800 p-1"
+        >
+          Add Todo
+        </button>
+      </div>
+      <div className="w-3/12">
+        {Object.keys(todoList).length > 0 &&
+          Object.keys(todoList).map((todoKey, idx) => {
+            const todoValue = todoList[todoKey];
+            return todoComponent(todoKey, todoValue, idx, handleRadioClick);
+          })}
+      </div>
+    </div>
+  );
+}
+
+const selectedTodoStyles = `bg-slate-700`;
+const unselectedTodoStyles = `bg-slate-800`;
+
+const todoComponent = (
+  todoKey: string,
+  todoValue: boolean,
+  idx: number,
+  handleRadioClick: () => void
+) => {
+  return (
+    <div>
+      <input type="radio" onClick={handleRadioClick} />
+      <div
+        className={`w-full mb-1 flex p-2 ${
+          todoValue ? selectedTodoStyles : unselectedTodoStyles
+        }`}
+        key={`${todoKey}-${idx}`}
+      >
+        {todoKey}
+      </div>
+    </div>
+  );
+};
