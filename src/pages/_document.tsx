23d5ac54615a5ab622a6d06e99c6d5ee1c0f8bd2
AlexArchibeque <wonkman33@gmail.com>
2023-04-16 13:05:28 -0700

    navbar update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import Link from 'next/link'
 
-const linkStyles = "bg-slate-700 p-4 flex items-center rounded-md"
+const linkStyles = "bg-slate-700 p-4 flex items-center rounded-md hover:bg-slate-800"
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <nav className="flex gap-4 border-b-2 border-white bg-blue-500">
+        <nav className="flex gap-4 border-b-2 border-white bg-blue-500 p-2">
           <Link href="/" className={linkStyles}>Home</Link>
           <Link href="/todo" className={linkStyles}>Todo list</Link>
           <Link href="/fetch" className={linkStyles}>Fetch test</Link>
